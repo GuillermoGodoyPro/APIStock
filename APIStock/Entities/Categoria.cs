2d93562410f9a78af2b5530c0f7ec37c3d4ca4cd
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-04-10 00:32:15 -0300

    Agregar archivos de proyecto.

---
+++ APIStock/Entities/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace APIStock.Entities
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string Nombre { get; set; }
+        public bool EsDestacada { get; set; }
+
+        public int IdProducto { get; set; }
+        public ICollection<Producto> ProductoReferencia { get; set; }
+
+    }
+}
---
+++ APIStock/Entities/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace APIStock.Entities
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public int IdCategoria { get; set; }
+        public virtual Categoria CategoriaReferencia { get; set; }  
+    }
+}
